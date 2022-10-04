517f93172cf21ff27eee28853ca365e94a8e5248
1257198442 <1257198442@qq.com>
2022-10-04 15:24:11 +0200

    adderd member variables and methods for the company class #2

--- src/main/java/Company.java
+++ src/main/java/Company.java
@@ -1,3 +1,48 @@
 public class Company {
+    private String addressCompany;
+    private Integer idCompany;
+    private String nameCompany;
 
+
+    public Company() {
+    }
+
+    public Company(String addressCompany, Integer idCompany, String nameCompany) {
+        this.addressCompany = addressCompany;
+        this.idCompany = idCompany;
+        this.nameCompany = nameCompany;
+    }
+
+    public String getAddressCompany() {
+        return addressCompany;
+    }
+
+    public void setAddressCompany(String addressCompany) {
+        this.addressCompany = addressCompany;
+    }
+
+    public Integer getIdCompany() {
+        return idCompany;
+    }
+
+    public void setIdCompany(Integer idCompany) {
+        this.idCompany = idCompany;
+    }
+
+    public String getNameCompany() {
+        return nameCompany;
+    }
+
+    public void setNameCompany(String nameCompany) {
+        this.nameCompany = nameCompany;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "addressCompany='" + addressCompany + '\'' +
+                ", idCompany=" + idCompany +
+                ", nameCompany='" + nameCompany + '\'' +
+                '}';
+    }
 }
